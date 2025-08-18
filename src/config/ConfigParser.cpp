fda9d90bc2e6f0abac1e3f4645c3aa38c51899a8
Daria Kolodzey <kolodaria@gmail.com>
2025-08-18 16:10:23 +0200

    Create logger to write messages

--- src/config/ConfigParser.cpp
+++ src/config/ConfigParser.cpp
@@ -6,15 +6,16 @@
 /*   By: qbeukelm <qbeukelm@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/08/11 09:39:08 by qbeukelm      #+#    #+#                 */
-/*   Updated: 2025/08/11 13:29:05 by dkolodze      ########   odam.nl         */
+/*   Updated: 2025/08/18 15:07:15 by dkolodze      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "config/ConfigParser.hpp"
+#include "log/Logger.hpp"
 
 #include <iostream>
 
 ConfigParser::ConfigParser()
 {
-	std::cout << "Created Parser\n";
+	Logger::debug("Created Parser");
 }
--- src/main.cpp
+++ src/main.cpp
@@ -6,16 +6,19 @@
 /*   By: qbeukelm <qbeukelm@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/08/11 09:26:29 by qbeukelm      #+#    #+#                 */
-/*   Updated: 2025/08/11 13:35:24 by dkolodze      ########   odam.nl         */
+/*   Updated: 2025/08/18 15:21:35 by dkolodze      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "config/ConfigParser.hpp"
+#include "log/Logger.hpp"
 
 int main(int argc, char **argv)
 {
 	(void)argc;
 	(void)argv;
 
-	ConfigParser();
+	Logger::setLogLevel(LOG_LEVEL_DEBUG);
+
+	ConfigParser parser;
 }
